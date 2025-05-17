70be7326824c10cf961c6df56c63ebd7bb751e34
Zachary Thomas <zachthomas2025@gmail.com>
2025-05-17 19:11:40 -0400

    Add max health field, rebalance stats, and add configurations for two and three players

--- sql/setup.sql
+++ sql/setup.sql
@@ -9,6 +9,7 @@ CREATE TYPE entity_type AS ENUM ('none', 'ship', 'base');
 CREATE TABLE ships (
   ship_id SERIAL PRIMARY KEY,
   name VARCHAR(50),
+  "maxHealth" INT,
   health INT,
   speed INT,
   damage INT,
@@ -19,6 +20,7 @@ CREATE TABLE ships (
 -- Create the players table which holds player information
 CREATE TABLE bases (
   base_id SERIAL PRIMARY KEY,
+  "maxHealth" INT,
   health INT,
 	player INT
 );
@@ -33,20 +35,19 @@ CREATE TABLE board (
 );
 
 -- Insert all the unique ships for each player into the ships table
-INSERT INTO ships (name, health, speed, damage, range, player)
+INSERT INTO ships (name, "maxHealth", health, speed, damage, range, player)
 VALUES
-  ('Light Ship', 3, 2, 2, 2, 1),
-  ('Heavy Ship', 5, 1, 3, 3, 2),
-  ('Glass Cannon', 2, 1, 5, 2, 3),
-  ('Light Ship', 3, 2, 2, 2, 4);
+  ('Light Ship', 8, 8, 2, 3, 2, 1),
+  ('Heavy Ship', 10, 10, 1, 4, 3, 2),
+  ('Glass Cannon', 5, 5, 1, 5, 2, 3);
 
 -- Insert all the bases into the bases table
-INSERT INTO bases (health, player)
+INSERT INTO bases ("maxHealth", health, player)
 VALUES
-  (2, 1),
-  (2, 2),
-  (2, 3),
-  (2, 4);
+  (5, 5, 1),
+  (5, 5, 2),
+  (5, 5, 3),
+  (5, 5, 4);
 
 -- Insert all the positions on the board into the board table
 DO $$
@@ -63,12 +64,30 @@ BEGIN
 END $$;
 
 -- Update the board table to reflect the initial positions of the players
+
+-- Four Players
+-- UPDATE board SET entity_type = 'ship', entity_id = 1 WHERE x_coord = 'B' AND y_coord = 2;
+-- UPDATE board SET entity_type = 'ship', entity_id = 2 WHERE x_coord = 'B' AND y_coord = 10;
+-- UPDATE board SET entity_type = 'ship', entity_id = 3 WHERE x_coord = 'I' AND y_coord = 1;
+-- UPDATE board SET entity_type = 'ship', entity_id = 4 WHERE x_coord = 'I' AND y_coord = 9;
+
+-- UPDATE board SET entity_type = 'base', entity_id = 1 WHERE x_coord = 'A' AND y_coord = 1;
+-- UPDATE board SET entity_type = 'base', entity_id = 2 WHERE x_coord = 'A' AND y_coord = 10;
+-- UPDATE board SET entity_type = 'base', entity_id = 3 WHERE x_coord = 'J' AND y_coord = 1;
+-- UPDATE board SET entity_type = 'base', entity_id = 4 WHERE x_coord = 'J' AND y_coord = 10;
+
+-- Three Players
 UPDATE board SET entity_type = 'ship', entity_id = 1 WHERE x_coord = 'B' AND y_coord = 2;
-UPDATE board SET entity_type = 'ship', entity_id = 2 WHERE x_coord = 'B' AND y_coord = 10;
-UPDATE board SET entity_type = 'ship', entity_id = 3 WHERE x_coord = 'I' AND y_coord = 1;
-UPDATE board SET entity_type = 'ship', entity_id = 4 WHERE x_coord = 'I' AND y_coord = 9;
+UPDATE board SET entity_type = 'ship', entity_id = 2 WHERE x_coord = 'I' AND y_coord = 1;
+UPDATE board SET entity_type = 'ship', entity_id = 3 WHERE x_coord = 'E' AND y_coord = 9;
 
 UPDATE board SET entity_type = 'base', entity_id = 1 WHERE x_coord = 'A' AND y_coord = 1;
-UPDATE board SET entity_type = 'base', entity_id = 2 WHERE x_coord = 'A' AND y_coord = 10;
-UPDATE board SET entity_type = 'base', entity_id = 3 WHERE x_coord = 'J' AND y_coord = 1;
-UPDATE board SET entity_type = 'base', entity_id = 4 WHERE x_coord = 'J' AND y_coord = 10;
+UPDATE board SET entity_type = 'base', entity_id = 2 WHERE x_coord = 'J' AND y_coord = 1;
+UPDATE board SET entity_type = 'base', entity_id = 3 WHERE x_coord = 'E' AND y_coord = 10;
+
+-- Two Players
+-- UPDATE board SET entity_type = 'ship', entity_id = 1 WHERE x_coord = 'B' AND y_coord = 2;
+-- UPDATE board SET entity_type = 'ship', entity_id = 3 WHERE x_coord = 'I' AND y_coord = 9;
+
+-- UPDATE board SET entity_type = 'base', entity_id = 1 WHERE x_coord = 'A' AND y_coord = 1;
+-- UPDATE board SET entity_type = 'base', entity_id = 3 WHERE x_coord = 'J' AND y_coord = 10;
